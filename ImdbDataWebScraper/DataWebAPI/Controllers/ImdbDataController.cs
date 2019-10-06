b195824ec769e6d115571490aa65616815a8b888
srohinireddy <srohinireddy@gmail.com>
2019-10-06 02:20:22 +0200

    testing

--- ImdbDataWebScraper/DataWebAPI/Controllers/ImdbDataController.cs
+++ ImdbDataWebScraper/DataWebAPI/Controllers/ImdbDataController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Downloader;
+using Parser;
+using Common;
 namespace DataWebAPI.Controllers
 {
     [ApiController, Route("~/")]
@@ -24,7 +26,18 @@ namespace DataWebAPI.Controllers
 			var startTime = DateTime.Now;
 			// TODO: implement scraper and parser logic
 			var totalTime = startTime - DateTime.Now;
-            Downloader.DatasetDownloader downloader = new DatasetDownloader();
+            //DatasetDownloader downloader = new DatasetDownloader();
+            DatasetParser parser = new DatasetParser();
+            parser.Process(StringConstants.akasFile);
+            parser.Process(StringConstants.crewFile);
+            parser.Process(StringConstants.episodeFile);
+            parser.Process(StringConstants.imdbDatasetURL);
+            parser.Process(StringConstants.nameBasicsFile);
+            parser.Process(StringConstants.principalsFile);
+            parser.Process(StringConstants.ratingsFile);
+
+            var dataset = parser.GetIMDBDataset();
+            
 			return Ok($"Done! Handler duration: {totalTime.Duration()}");
 		}
     }
